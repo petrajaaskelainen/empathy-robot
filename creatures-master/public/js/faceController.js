e23ebe7ac926c72735b5cf421c416f3212b7feea
Petra J <jaaskelainen.petra@gmail.com>
2019-03-01 22:59:56 +0100

    Add files via upload

---
+++ creatures-master/public/js/faceController.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+$(document).ready(function(){
+
+	//When clicking the button, the specific face is shown.	
+	$("#angryc").click(function(){
+		$("#angry").show();
+	}); 
+	
+	$("#aneutralc").click(function(){
+		$("#neutral").show();
+	}); 	
+	
+});	
